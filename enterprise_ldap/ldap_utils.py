91e05c5c187e00ea3720cf2b0ecaac09064620e3
鸿昊 <honghao@dian.so>
2020-03-08 01:38:12 +0800

    modify chinese name filed

--- enterprise_ldap/ldap_utils.py
+++ enterprise_ldap/ldap_utils.py
@@ -19,7 +19,19 @@ class SearchLdap:
       #与ldap建立连接
       s = Server(host=self.host, port=self.port, use_ssl=False, get_info='ALL', connect_timeout=5)
       #bind打开连接
-      c = Connection(s, user=ldap_user, password=password, auto_bind='NONE', version=3, authentication='SIMPLE', client_strategy='SYNC', auto_referrals=True, check_names=True, read_only=True, lazy=False, raise_exceptions=False)
+      c = Connection(
+              s,
+              user=ldap_user,
+              password=password,
+              auto_bind='NONE',
+              version=3,
+              authentication='SIMPLE',
+              client_strategy='SYNC',
+              auto_referrals=True,
+              check_names=True,
+              read_only=True,
+              lazy=False,
+              raise_exceptions=False)
 
       c.bind()
       logger.info(c.result)
@@ -28,7 +40,7 @@ class SearchLdap:
         res = c.search(search_base=self.ldap_base, search_filter = "(cn="+username+")", search_scope = SUBTREE, attributes = ['cn', 'mobile', 'mail'], paged_size = 5)
         if res:
           attr_dict = c.response[0]["attributes"]
-          chname = attr_dict['cn'][0]
+          chname = attr_dict['givenName'][0]
           email = attr_dict['mail'][0]
           mobile = attr_dict['mobile'][0]
           data = {
